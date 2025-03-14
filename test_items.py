4ce316c24f90514729892f74924cffcfac4936f8
psPash15 <snopkov.2012@gmail.com>
2025-03-14 12:28:07 +0300

    assert changed

--- test_items.py
+++ test_items.py
@@ -7,4 +7,4 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 def test_start_browser_with_language(browser):
     browser.get(link)
     button=browser.find_element(By.CLASS_NAME, "btn.btn-lg.btn-primary.btn-add-to-basket")
-    assert button != True, "Корзина не найдена"
+    assert button != True, "Basket not found"
